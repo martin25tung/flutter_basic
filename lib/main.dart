98443a24b4a051032052b617e79d9fc5511c6876
Martin Tung <martin.tung@metropia.com>
2022-11-09 09:47:36 +0800

    按鈕Button

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class HomePage extends StatelessWidget {
           elevation: 10,
           centerTitle: true,
         ),
-        body: TextDemo());
+        body: ButtonDemo());
   }
 }
 
--- lib/widgets/demo_01.dart
+++ lib/widgets/demo_01.dart
@@ -19,3 +19,37 @@ class TextDemo extends StatelessWidget {
     );
   }
 }
+
+class ButtonDemo extends StatelessWidget {
+  const ButtonDemo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(onPressed: null, child: Text("漂浮按鈕")),
+        TextButton(
+          onPressed: () {},
+          child: Text("扁平按鈕"),
+        ),
+        TextButton.icon(
+          onPressed: () {},
+          label: Text(
+            "data",
+            style: TextStyle(color: Colors.white),
+          ),
+          icon: Icon(Icons.add),
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.green),
+              foregroundColor: MaterialStateProperty.all(Colors.white)),
+        ), // FlatButton 改成用TextButton
+        OutlinedButton(
+            onPressed: () {}, child: Text("OutlineButton")), // 有邊筐沒有陰影
+        IconButton(
+          icon: Icon(Icons.home),
+          onPressed: () {},
+        )
+      ],
+    );
+  }
+}
