6a70afa1f89dae98050e34ec6a7b29cc16280532
Martin Tung <martin.tung@metropia.com>
2022-11-14 11:58:51 +0800

    路由進階用法

--- lib/widgets/navigator_demo.dart
+++ lib/widgets/navigator_demo.dart
@@ -12,9 +12,14 @@ class LoginPage extends StatelessWidget {
       ),
       body: ElevatedButton(
         onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-            return MenuPage(); // 跳轉到菜單
-          }));
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) {
+              return MenuPage(); // 跳轉到菜單
+            },
+            maintainState: false, // 是否釋放資源
+            settings: RouteSettings(name: "menu", arguments: ""), //設置路由訊息，是不是首頁
+            fullscreenDialog: false, // 變成全螢幕Dialog
+          ));
         },
         child: Text("登入"),
       ),
