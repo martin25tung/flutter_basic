583b681081e4959453cb8afe938bbc3213c9dbdf
Martin Tung <martin.tung@metropia.com>
2022-11-14 14:18:34 +0800

    路由傳值

--- lib/widgets/navigator_demo.dart
+++ lib/widgets/navigator_demo.dart
@@ -11,16 +11,18 @@ class LoginPage extends StatelessWidget {
         centerTitle: true,
       ),
       body: ElevatedButton(
-        onPressed: () async {
-          var result = await Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) {
-              return MenuPage(title: "菜單"); // 跳轉到菜單
-            },
-            maintainState: false, // 是否釋放資源
-            settings: RouteSettings(name: "menu", arguments: ""), //設置路由訊息，是不是首頁
-            fullscreenDialog: false, // 變成全螢幕Dialog
-          ));
-          print(result);
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(
+                builder: (context) {
+                  return MenuPage(title: "菜單"); // 跳轉到菜單
+                },
+                maintainState: false, //  是否釋放資源
+                settings: RouteSettings(
+                    name: "menu", arguments: {"name": "123212"}), //設置路由訊息，是不是首頁
+                fullscreenDialog: false, // 變成全螢幕Dialog
+              ))
+              .then((value) => print(value));
         },
         child: Text("登入"),
       ),
@@ -31,12 +33,13 @@ class LoginPage extends StatelessWidget {
 class MenuPage extends StatelessWidget {
   const MenuPage({Key? key, required this.title}) : super(key: key);
   final String title;
-
   @override
   Widget build(BuildContext context) {
+    dynamic arguments = ModalRoute.of(context)?.settings.arguments;
+    print(arguments);
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title + " " + arguments.toString()),
         centerTitle: true,
       ),
       body: ElevatedButton(
