9964dcf149e1c151b2891c4b3268d96cf8f6fec1
Martin Tung <martin.tung@metropia.com>
2022-11-16 16:28:33 +0800

    流式佈局 Warp

--- lib/widgets/layout_demo.dart
+++ lib/widgets/layout_demo.dart
@@ -54,36 +54,76 @@ class LayoutDemo extends StatelessWidget {
         //     )
         //   ],
         // ),
-        child: Flex(
-          direction: Axis.vertical,
-          children: [
-            Expanded(
-              child: Container(
-                color: Colors.red,
-                width: 100,
-                height: 200,
-              ),
-              flex: 1,
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.green,
-                width: 100,
-                height: 100,
-              ),
-              flex: 2,
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.yellow,
-                width: 100,
-                height: 100,
-              ),
-              flex: 1,
-            )
-          ],
-        ),
+        // child: Flex(
+        //   direction: Axis.vertical,
+        //   children: [
+        //     Expanded(
+        //       child: Container(
+        //         color: Colors.red,
+        //         width: 100,
+        //         height: 200,
+        //       ),
+        //       flex: 1,
+        //     ),
+        //     Expanded(
+        //       child: Container(
+        //         color: Colors.green,
+        //         width: 100,
+        //         height: 100,
+        //       ),
+        //       flex: 2,
+        //     ),
+        //     Expanded(
+        //       child: Container(
+        //         color: Colors.yellow,
+        //         width: 100,
+        //         height: 100,
+        //       ),
+        //       flex: 1,
+        //     )
+        //   ],
+        // ),
+        child: WrapDemo(),
       ),
+    );
+  }
+}
+
+class WrapDemo extends StatefulWidget {
+  const WrapDemo({Key? key}) : super(key: key);
+
+  @override
+  State<WrapDemo> createState() => _WrapDemoState();
+}
+
+class _WrapDemoState extends State<WrapDemo> {
+  List<int> list = [];
+
+  @override
+  void initState() {
+    super.initState();
+    for (var i = 0; i < 20; i++) {
+      list.add(i);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      direction: Axis.vertical,
+      alignment: WrapAlignment.spaceEvenly,
+      spacing: 1.0,
+      // 縱軸
+      runSpacing: 1.0,
+      //橫軸
+      children: list
+          .map((e) => Container(
+                height: 100,
+                width: 100,
+                child: Text(e.toString()),
+                color: Colors.blue,
+              ))
+          .toList(),
     );
   }
 }
