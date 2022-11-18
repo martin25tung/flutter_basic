065ddd260d827cc07fea641d2e622024d76969ed
Martin Tung <martin.tung@metropia.com>
2022-11-18 17:18:43 +0800

    底部選項卡BottomNavigationBar進階

--- lib/widgets/bottom_navigator.dart
+++ lib/widgets/bottom_navigator.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'demo_04.dart';
+import 'layout_demo.dart';
+
 class BottomNavigatorBarDemo extends StatefulWidget {
-  const BottomNavigatorBarDemo({Key? key}) : super(key: key);
+  List<Widget> widgets = [PageDemo(), LayoutDemo()];
 
   @override
   State<BottomNavigatorBarDemo> createState() => _BottomNavigatorBarDemoState();
@@ -14,12 +17,15 @@ class _BottomNavigatorBarDemoState extends State<BottomNavigatorBarDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("底部選項卡"),
-        centerTitle: true,
-      ),
+      // appBar: AppBar(
+      //   title: Text("底部選項卡"),
+      //   centerTitle: true,
+      // ),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed, // 超過3個，就需要用這個參數
         items: [
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: "新增"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "我的"),
           BottomNavigationBarItem(icon: Icon(Icons.add), label: "新增"),
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "我的")
         ],
@@ -30,9 +36,26 @@ class _BottomNavigatorBarDemoState extends State<BottomNavigatorBarDemo> {
           });
         },
       ),
-      body: Center(
-        child: Text(_index.toString()),
+      // bottomNavigationBar: BottomAppBar(
+      //   color: Theme.of(context).primaryColor,
+      //   shape: CircularNotchedRectangle(),
+      //   child: Row(
+      //     mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //     children: [
+      //       IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+      //       SizedBox(),
+      //       IconButton(onPressed: () {}, icon: Icon(Icons.home)),
+      //     ],
+      //   ),
+      // ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("object");
+        },
+        child: Icon(Icons.widgets_rounded),
       ),
+      body: widget.widgets[_index],
     );
   }
 }
