3750afab9b9e0906d385885575fa014fdfaf7eab
Martin Tung <martin.tung@metropia.com>
2022-11-22 11:04:20 +0800

    表格進階選擇匡、內容排序

--- lib/widgets/table_demo.dart
+++ lib/widgets/table_demo.dart
@@ -10,12 +10,19 @@ class TableDemo extends StatefulWidget {
 
 class _TableDemoState extends State<TableDemo> {
   List<Map> list = [];
+  int _sortColumnIndex = 0;
+  bool _sortAscending = true;
 
   @override
   void initState() {
     super.initState();
-    for (var i = 0; i < 10; i++) {
-      list.add({"name": "王" + i.toString(), "gender": i % 1 == 0 ? "男" : "女"});
+    for (var i = 1; i < 10; i++) {
+      list.add({
+        "name": "王" * i,
+        "gender": i % 1 == 0 ? "男" : "女",
+        "select": false,
+        "age": i
+      });
     }
   }
 
@@ -36,15 +43,72 @@ class _TableDemoState extends State<TableDemo> {
           //       .toList(),
           // ),
           child: DataTable(
+            // onSelectAll: (v) {
+            //   for (var i = 0; i < list.length; i++) {
+            //     setState(() {
+            //       if (list[i]["select"] != v) {
+            //         list[i]["select"] = v;
+            //       }
+            //     });
+            //   }
+            // }, // 自己實現select all 的功能，不推薦使用
+            sortColumnIndex: _sortColumnIndex,
+            sortAscending: _sortAscending,
             columns: [
-              DataColumn(label: Text("姓名")),
-              DataColumn(label: Text("性別"))
+              DataColumn(
+                  label: Text("姓名"),
+                  onSort: (columnIndex, ascending) {
+                    setState(() {
+                      // 對名字做排序
+                      _sortColumnIndex = columnIndex;
+                      _sortAscending = ascending;
+                      list.sort((a, b) {
+                        if (!ascending) {
+                          var c = a;
+                          a = b;
+                          b = c;
+                        }
+                        return a["name"]
+                            .toString()
+                            .length
+                            .compareTo(b["name"].toString().length);
+                      });
+                    });
+                  }),
+              DataColumn(
+                  label: Text("年齡"),
+                  onSort: (columnIndex, ascending) {
+                    setState(() {
+                      // 對年齡做排序
+                      _sortColumnIndex = columnIndex;
+                      _sortAscending = ascending;
+                      list.sort((a, b) {
+                        if (!ascending) {
+                          var c = a;
+                          a = b;
+                          b = c;
+                        }
+                        return a["age"].compareTo(b["age"]);
+                      });
+                    });
+                  }),
+              DataColumn(label: Text("性別")),
             ],
             rows: list
-                .map((e) => DataRow(cells: [
-                      DataCell(Text(e["name"])),
-                      DataCell(Text(e["gender"]))
-                    ]))
+                .map((e) => DataRow(
+                        selected: e["select"],
+                        onSelectChanged: (v) {
+                          setState(() {
+                            if (e["select"] != v) {
+                              e["select"] = v;
+                            }
+                          });
+                        },
+                        cells: [
+                          DataCell(Text(e["name"])),
+                          DataCell(Text(e["age"].toString())),
+                          DataCell(Text(e["gender"]))
+                        ]))
                 .toList(),
           )),
     );
